1f59f981630f2c7a20521caaa3aa745e3b00d080
ozvadez <sungwemukuka@gmail.com>
2019-07-30 21:17:30 +0200

    added home, about, contact views

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/', function () {
+    return view('about');
+});
+
+Route::get('/', function () {
+    return view('contact');
 });
