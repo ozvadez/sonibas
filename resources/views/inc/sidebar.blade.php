f71b17694f67aa205be7dbe4eecba1b4ff82ead5
ozvadez <sungwemukuka@gmail.com>
2019-08-28 11:53:42 +0200

    sidebar

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,7 +1,7 @@
 @section('sidebar')
     <div class="card mb-4 shadow-sm">
         <div class="card-header">
-                <h3>Sidebar</h3>
+            <h3>Sidebar</h3>
         </div>
         <div class="card-body">
             <p>This is the side bar</p>
